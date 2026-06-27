24fef68bf20c64501e7ccf24505502af5b6f67fb
raghavgit9-sketch <raghav.git9@gmail.com>
2026-06-27 12:56:48 +0530

    14th Sept Part 1

---
+++ src/ex_02_Java_Basics/Lab024_printF.java
@@ -0,0 +1,27 @@
+package ex_02_Java_Basics;
+
+public class Lab024_printF {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+//        System.out.println(a);
+//        System.out.println(b);
+
+//        System.out.printf("Value of a is %d",a);
+//        System.out.println();
+//        System.out.printf("Value of b is %d",b);
+
+        System.out.printf("Value of a=%d",a);
+        System.out.println();
+        System.out.printf("Value of b=%d",b);
+
+        // %d -> int, byte, long, short, - data type
+        // %s -> String
+        // %f -> float, double
+        // %b -> boolean
+
+    }
+}
---
+++ src/ex_02_Java_Basics/Table_of_9.java
@@ -0,0 +1,24 @@
+package ex_02_Java_Basics;
+
+public class Table_of_9 {
+
+    public static void main(String[] args) {
+
+        int table = 9;
+
+        System.out.println("Print the table of 9");
+
+        System.out.printf("%d * 1 = %d %n",table,table*1);
+        System.out.printf("%d * 2 = %d %n",table,table*2);
+        System.out.printf("%d * 3 = %d %n",table,table*3);
+        System.out.printf("%d * 4 = %d %n",table,table*4);
+        System.out.printf("%d * 5 = %d %n",table,table*5);
+        System.out.printf("%d * 6 = %d %n",table,table*6);
+        System.out.printf("%d * 7 = %d %n",table,table*7);
+        System.out.printf("%d * 8 = %d %n",table,table*8);
+        System.out.printf("%d * 9 = %d %n",table,table*9);
+        System.out.printf("%d * 10 = %d %n",table,table*10);
+
+
+    }
+}
