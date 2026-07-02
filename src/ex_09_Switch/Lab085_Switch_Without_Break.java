33d5fb7ea4984e3c14a361e500851af243ad53ce
raghavgit9-sketch <raghav.git9@gmail.com>
2026-07-02 05:52:07 +0530

    27

---
+++ src/ex_09_Switch/Lab085_Switch_Without_Break.java
@@ -0,0 +1,31 @@
+package ex_09_Switch;
+
+import java.util.Scanner;
+
+public class Lab085_Switch_Without_Break {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the day to 1 to 7");
+        int day = scanner.nextInt();
+
+        switch (day){
+            case 1:
+                System.out.println("Mon");
+            case 2:
+                System.out.println("Tue");
+            case 3:
+                System.out.println("Wed");
+            case 4:
+                System.out.println("Thr");
+            case 5:
+                System.out.println("Fri");
+            case 6:
+                System.out.println("Sat");
+            case 7:
+                System.out.println("Sun");
+            default:
+                System.out.println("Invalid, day number");
+        }
+    }
+}
---
+++ src/ex_09_Switch/Lab087_Real_Switch_Automation.java
@@ -0,0 +1,16 @@
+package ex_09_Switch;
+
+import java.util.Scanner;
+
+public class Lab087_Real_Switch_Automation {
+    public static void main(String[] args) {
+
+        // Web Automation
+        // I will ask the user to give me the input from browser which he wants to
+        // use to I will start the automation in that browser
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the browser");
+        String browser = scanner.next();
+    }
+}
